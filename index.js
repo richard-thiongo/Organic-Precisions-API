7d83e5a4f3a3d49ded2867971d9535c4613d6900
richard-thiongo <thiongorichard0@gmail.com>
2026-05-19 10:53:15 +0300

    updated the vulnerables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const { pool } = require('./src/shared/config/db');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const startServer = async () => {
   try {
